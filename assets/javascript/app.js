0cd471bf3f85ec44e6f4d2f4e68785fecd7c5f5d
sajad1993gh <sajad1993gh@gmail.com>
2019-06-05 17:24:07 -0400

    pushing HTML CSS

---
+++ assets/javascript/app.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+
+
+
+
+
+
+
+    function winPage() {
+        $(".gif-screen").html("<img src='https://media.giphy.com/media/6brH8dM3zeMyA/giphy.gif' class='winGif'>");
+    }
+
+    function losePage() {
+        $(".gif-screen").html("<img src='https://media.giphy.com/media/26FeWWOMsz7cWeYcU/giphy.gif' class='loseGif'>");
+    }
+
+    function endPage() {
+        $(".gif-screen").html("<img src='https://media.giphy.com/media/26AHC0kdj8IeLkmBy/giphy.gif' class='endGif'>");
+    }
+
+
+
+
+
+
+
+
+
+
+});
