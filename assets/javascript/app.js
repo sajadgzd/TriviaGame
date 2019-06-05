f1d18447cc7239180bced057ffbc23709ecea215
sajad1993gh <sajad1993gh@gmail.com>
2019-06-05 17:56:33 -0400

    developing Js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,38 +4,27 @@ $(document).ready(function() {
         question: "During what event did Evan have his first black-out?",
         answerChoices: ["A. Meeting his father for the first time", "B. Drawing a picture in school", "C. Being abused by Kayleigh and Tommy's father", "D. Finding a knife in the kitchen"],
         answer: "B.",
-
     }
-
     var q1 = {
         question: "What is odd about the mailbox in which Lenny puts the blockbuster?",
         answerChoices: ["A. It has the wrong address on it", "B. It looks just like the house for which it holds the mail", "C. It is made out of an old bird-feeder", "D. It makes noise when it is opened"],
         answer: "B.",
-
     }
-
     var q2 = {
         question: "What does Tommy do to get himself thrown out of the theatre?",
         answerChoices: ["A. He beats up another patron", "B. He snuck into an R-rated movie", "C. He curses at Evan for kissing Kayleigh", "D. He wouldn't shut up during the movie"],
         answer: "A.",
-
     }
-
-
     var q3 = {
         question: "What is Evan's dog's name?",
         answerChoices: ["A. Jason", "B. Fido", "C. Crocket", "D. Tommy"],
         answer: "C.",
-
     }
-
     var q4 = {
         question: "Why does Evan want to go out and celebrate with Thumper?",
         answerChoices: ["A. It's Saturday. No more reasoning required", "B. He aced another mid-term", "C. He messed up another bell curve on an exam", "D. Seven years and no black-outs"],
         answer: "D.",
-
     }
-
     var timeLeft = 10;
     var losses = 0;
     var wins = 0;
@@ -45,7 +34,6 @@ $(document).ready(function() {
     var questions = [q0.question, q1.question, q2.question, q3.question, q4.question];
     var answerOptions = [q0.answerChoices, q1.answerChoices, q2.answerChoices, q3.answerChoices, q4.answerChoices];
     var answers = [q0.answer, q1.answer, q2.answer, q3.answer, q4.answer]
-
 
     function winPage() {
         $(".gif-screen").html("<img src='https://media.giphy.com/media/6brH8dM3zeMyA/giphy.gif' class='winGif'>");
@@ -98,8 +86,6 @@ $(document).ready(function() {
     $(".restart").on("click", function() {
         reset();
     });
-
-
 
 
 
