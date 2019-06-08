a0bd08b96657cada0b06a5efcf94f5ec56995922
sajad1993gh <sajad1993gh@gmail.com>
2019-06-08 15:37:13 -0400

    developed buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,27 +3,27 @@ $(document).ready(function() {
     var q0 = {
         question: "During what event did Evan have his first black-out?",
         answerChoices: ["A. Meeting his father for the first time", "B. Drawing a picture in school", "C. Being abused by Kayleigh and Tommy's father", "D. Finding a knife in the kitchen"],
-        answer: "B.",
+        answer: "B. Drawing a picture in school",
     }
     var q1 = {
         question: "What is odd about the mailbox in which Lenny puts the blockbuster?",
         answerChoices: ["A. It has the wrong address on it", "B. It looks just like the house for which it holds the mail", "C. It is made out of an old bird-feeder", "D. It makes noise when it is opened"],
-        answer: "B.",
+        answer: "B. It looks just like the house for which it holds the mail",
     }
     var q2 = {
         question: "What does Tommy do to get himself thrown out of the theatre?",
         answerChoices: ["A. He beats up another patron", "B. He snuck into an R-rated movie", "C. He curses at Evan for kissing Kayleigh", "D. He wouldn't shut up during the movie"],
-        answer: "A.",
+        answer: "A. He beats up another patron",
     }
     var q3 = {
         question: "What is Evan's dog's name?",
         answerChoices: ["A. Jason", "B. Fido", "C. Crocket", "D. Tommy"],
-        answer: "C.",
+        answer: "C. Crocket",
     }
     var q4 = {
         question: "Why does Evan want to go out and celebrate with Thumper?",
         answerChoices: ["A. It's Saturday. No more reasoning required", "B. He aced another mid-term", "C. He messed up another bell curve on an exam", "D. Seven years and no black-outs"],
-        answer: "D.",
+        answer: "D. Seven years and no black-outs",
     }
     var timeLeft = 3; // # of seconds left
     var losses = 0; // # of wrong answers
@@ -67,11 +67,45 @@ $(document).ready(function() {
 
     function game() {
         if (questionNumber < questions.length) {
+            $(".gif-screen").empty();
+            $(".results").empty();
+            timeLeft = 3;
             intervalId = setInterval(countdown, 1000);
+            $(".question").html("<h2>" + questions[questionNumber] + "</h2>");
+            for (let i = 0; i < answerOptions[questionNumber].length; i++) {
+                $(".question").append(`<button class='buttons button${[i]}' value='${answerOptions[questionNumber][i]}'> ${answerOptions[questionNumber][i]} </button>`);
+            }
+
+            $(".buttons").on("click", function() {
+                console.log($(this).attr("value"));
+                console.log(answers[questionNumber]);
+                if ($(this).val() === answers[questionNumber]) {
+
+                    clearInterval(intervalId);
+                    $(".results").html("That's Correct! The answer is: <br> " + answers[questionNumber]);
+                    winPage();
+                    wins++;
+                    questionNumber++;
+                    setTimeout(game, 3000);
+                } else {
+                    clearInterval(intervalId);
+                    $(".results").html("Wrong! The correct answer is: <br>" + answers[questionNumber]);
+                    losePage();
+                    losses++;
+                    questionNumber++;
+                    setTimeout(game, 3000);
+                }
+
+            });
+
+
 
 
         } else {
+            clearInterval(intervalId);
+            $(".results").text("Game Over! Press Restart to Play Again!");
 
+            endPage();
         }
 
     }
